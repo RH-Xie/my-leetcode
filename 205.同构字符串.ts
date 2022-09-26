d03fe7e23f5b473903e6b12c530a9afba8a34996
RH-Xie <1748421646@qq.com>
2022-09-26 23:40:46 +0800

    2022.9.26

--- 205.同构字符串.ts
+++ 205.同构字符串.ts
@@ -6,7 +6,8 @@
 
 // @lc code=start
 function isIsomorphic(s: string, t: string): boolean {
-  // !!!
+  // 哈希映射，“用他们的办法解决他们的问题”
+  // 即使用哈希表建立映射关系，存在key的话就get对应的value，等则同构，不等则不同构
   let map: Map<string, string> = new Map();
     let set: Set<string> = new Set();
     for (let i = 0; i < s.length; i++) {
