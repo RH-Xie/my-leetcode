3ec41d57520cd594c9049ed8c857a6a429570ded
RH-Xie <1748421646@qq.com>
2022-10-05 23:33:52 +0800

    2022.10.5

--- 13.罗马数字转整数.ts
+++ 13.罗马数字转整数.ts
@@ -6,7 +6,27 @@
 
 // @lc code=start
 function romanToInt(s: string): number {
-  return 0;
+  const map = new Map([
+    ['I', 1],
+    ['V', 5],
+    ['X', 10],
+    ['L', 50],
+    ['C', 100],
+    ['D', 500],
+    ['M', 1000],
+  ]);
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    const current = map.get(s[i]);
+    const next = map.get(s[i + 1]);
+    if (next && next > current) {
+      result += next - current;
+      i++;
+    } else {
+      result += current;
+    }
+  }
+  return result;
 };
 // @lc code=end
 
