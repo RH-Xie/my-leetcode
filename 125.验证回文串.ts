42cda857b676fa0930746c6e165a0af7d33d9162
RH-Xie <1748421646@qq.com>
2022-10-10 23:51:01 +0800

    2022.10.10

---
+++ 125.验证回文串.ts
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=125 lang=typescript
+ *
+ * [125] 验证回文串
+ */
+
+// @lc code=start
+function isPalindrome(s: string): boolean {
+  const str = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  const len = str.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - i - 1]) {
+      return false;
+    }
+  }
+  return true;  
+};
+// @lc code=end
+
