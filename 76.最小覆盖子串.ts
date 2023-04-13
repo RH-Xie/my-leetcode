4af954fd8e004a09ec353bad75fc247491181318
RH-Xie <1748421646@qq.com>
2023-04-13 10:32:18 +0800

    update: refactor 2023.4.13

--- 76.最小覆盖子串.ts
+++ 76.最小覆盖子串.ts
@@ -6,42 +6,46 @@
 
 // @lc code=start
 function minWindow(s: string, t: string): string {
-  let need : Map<string, number> = new Map();
-  let window : Map<string, number>  = new Map();
-  for(let c of t) {
-    need.set(c, (need.get(c) || 0) + 1);
-  }
-  let left = 0, right = 0;
-  let valid = 0;
-  let start = 0, len = Number.MAX_SAFE_INTEGER;
-  while(right < s.length) {
-    let c : string = s.charAt(right);
-    // 扩大窗口
-    right++;
-    if(need.has(c)) {
-      window.set(c, (window.get(c) || 0) + 1);
-      if(window.get(c) === need.get(c)) {
-        valid++;
-      }
+    let need: Map<string, number> = new Map();
+    let window: Map<string, number> = new Map();
+    for (let c of t) {
+        need.set(c, (need.get(c) || 0) + 1);
     }
-    while(valid === need.size) {
-      // 择优
-      if(right - left < len) {
-        start = left;
-        len = right - left;
-      }
-      let d : string = s.charAt(left);
-      // 收缩窗口
-      left++;
-      if(need.has(d)) {
-        if(window.get(d) === need.get(d)) {
-          valid--;
+    let left = 0,
+        right = 0;
+    let valid = 0;
+    let start = 0,
+        len = Number.MAX_SAFE_INTEGER;
+    while (right < s.length) {
+        let c: string = s.charAt(right);
+        // 扩大窗口
+        right++;
+        if (need.has(c)) {
+            window.set(c, (window.get(c) || 0) + 1);
+            if (window.get(c) === need.get(c)) {
+                valid++;
+            }
         }
-        window.set(d, window.get(d) - 1);
-      }
+        while (valid === need.size) {
+            // 择优
+            if (right - left < len) {
+                start = left;
+                len = right - left;
+            }
+            let d: string = s.charAt(left);
+            // 收缩窗口
+            left++;
+            if (need.has(d)) {
+                // valid本身含义是window和need内字符相同的个数，现在window里的要移出去了，自然就valid--了
+                if (window.get(d) === need.get(d)) {
+                    valid--;
+                }
+                window.set(d, window.get(d) - 1);
+            }
+        }
     }
-  }
-  return len === Number.MAX_SAFE_INTEGER ? '' : s.substring(start, start + len);
-};
+    return len === Number.MAX_SAFE_INTEGER
+        ? ""
+        : s.substring(start, start + len);
+}
 // @lc code=end
-
